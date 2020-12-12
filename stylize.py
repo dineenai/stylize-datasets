71ec882a54ccb46dbca273f5d841c28554d3f140
Áine T. Dineen <51408125+dineenai@users.noreply.github.com>
2020-12-12 17:42:06 +0000

    Update stylize.py

--- stylize.py
+++ stylize.py
@@ -34,7 +34,7 @@ parser.add_argument('--style-size', type=int, default=512,
                     keeping the original size if set to 0')
 parser.add_argument('--crop', type=int, default=0,
                     help='If != 0, center crop  applied to content image, \ 
-                    after resizing to create a squared image default: 0')
+                    after resizing to create a squared image default is 0')
 
 # random.seed(131213)
 
